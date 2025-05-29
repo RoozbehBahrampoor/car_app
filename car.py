fdcf906052425ea68fe9c126bcec3ecedf9c3920
rouzbeh_bahrampoor <roozbehbahrampoor18@gmail.com>
2025-05-29 17:39:41 +0330

    class

--- car.py
+++ car.py
@@ -18,17 +18,8 @@ class Car:
 car1 = Car("benz" , "gclass" , "red" , "2023" , "ali")
 car2 = Car("benz" , "cls" , "blue" , "2022" , "reza")
 
-print(car1.name)
-print(car1.model)
-print(car1.color)
-print(car1.production_date)
-print(car1.owner)
+
 car1.save()
-print(car2.name)
-print(car2.model)
-print(car2.color)
-print(car2.production_date)
-print(car2.owner)
 car2.save()
 
 
